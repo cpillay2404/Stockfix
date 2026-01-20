a4a53feeb37d6fc948b7a1b420d6d97b3cb4a293
carinpillay <51195498-carinpillay@users.noreply.replit.com>
2026-01-20 18:39:24 +0000

    Restored to '07bbb9130b94909c2584eea0fdb45334c1f7f38c'
    Replit-Restored-To: 07bbb9130b94909c2584eea0fdb45334c1f7f38c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,6 @@ import ExitVisit from "@/pages/exit-visit";
 import RepProgress from "@/pages/rep-progress";
 import ManagerProgress from "@/pages/manager-progress";
 import SelectManager from "@/pages/select-manager";
-import CompanyLeaderboard from "@/pages/company-leaderboard";
 
 function Router() {
   return (
@@ -33,7 +32,6 @@ function Router() {
       <Route path="/dashboard" component={Home} />
       <Route path="/tasks" component={TaskList} />
       <Route path="/import" component={ImportData} />
-      <Route path="/leaderboard" component={CompanyLeaderboard} />
       <Route path="/task/:id" component={TaskDetail} />
       <Route path="/store/:storeName" component={StoreSummary} />
       <Route path="/exit-visit" component={ExitVisit} />
